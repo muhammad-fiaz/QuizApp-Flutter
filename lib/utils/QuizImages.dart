b1f6af5d090e85be973ffbd7dfdb63e12d0ec049
muhammad-fiaz <contact@muhammadfiaz.com>
2024-01-18 18:14:32 +0530

    replaced the image from local dict

--- lib/utils/QuizImages.dart
+++ lib/utils/QuizImages.dart
@@ -1,16 +1,16 @@
 import 'package:quiz/utils/QuizConstant.dart';
 
-const quiz_ic_communication = "$BaseUrl/images/quiz/quiz_ic_communication.png";
-const quiz_ic_communication2 = "$BaseUrl/images/quiz/quiz_ic_communication2.png";
-const quiz_ic_course1 = "$BaseUrl/images/quiz/quiz_ic_course1.png";
-const quiz_ic_course2 = "$BaseUrl/images/quiz/quiz_ic_course2.png";
-const quiz_ic_course3 = "$BaseUrl/images/quiz/quiz_ic_course3.png";
-const quiz_ic_info = "$BaseUrl/images/quiz/quiz_ic_info.png";
-const quiz_ic_java = "$BaseUrl/images/quiz/quiz_ic_java.png";
-const quiz_ic_marketing = "$BaseUrl/images/quiz/quiz_ic_marketing.png";
-const quiz_ic_notification = "$BaseUrl/images/quiz/quiz_ic_notification.png";
-const quiz_ic_study1 = "$BaseUrl/images/quiz/quiz_ic_study1.png";
-const quiz_ic_study2 = "$BaseUrl/images/quiz/quiz_ic_study2.png";
+const quiz_ic_communication = "images/quiz/quiz_ic_communication.png";
+const quiz_ic_communication2 = "images/quiz/quiz_ic_communication2.png";
+const quiz_ic_course1 = "images/quiz/quiz_ic_course1.png";
+const quiz_ic_course2 = "images/quiz/quiz_ic_course2.png";
+const quiz_ic_course3 = "images/quiz/quiz_ic_course3.png";
+const quiz_ic_info = "images/quiz/quiz_ic_info.png";
+const quiz_ic_java = "images/quiz/quiz_ic_java.png";
+const quiz_ic_marketing = "images/quiz/quiz_ic_marketing.png";
+const quiz_ic_notification = "images/quiz/quiz_ic_notification.png";
+const quiz_ic_study1 = "images/quiz/quiz_ic_study1.png";
+const quiz_ic_study2 = "images/quiz/quiz_ic_study2.png";
 const quiz_ic_quiz1 = "images/quiz/quiz_ic_quiz1.png";
 const quiz_ic_quiz2 = "images/quiz/quiz_ic_quiz2.png";
 const quiz_ic_quiz = "images/quiz/quiz_ic_quiz.svg";
@@ -23,8 +23,8 @@ const quiz_ic_twitter = "images/quiz/quiz_ic_twitter.svg";
 
 const Quiz_ic_Grid = "images/quiz/quiz_ic_grid.png";
 const Quiz_ic_List = "images/quiz/quiz_ic_list.png";
-const quiz_img_People1 = "$BaseUrl/images/quiz/quiz_img_People1.jpg";
-const quiz_img_People2 = "$BaseUrl/images/quiz/quiz_img_People2.png";
+const quiz_img_People1 = "images/quiz/quiz_img_People1.jpg";
+const quiz_img_People2 = "images/quiz/quiz_img_People2.png";
 
 const quiz_ic_list1 = "images/quiz/quiz_ic_list1.png";
 const quiz_ic_list2 = "images/quiz/quiz_ic_list2.png";
